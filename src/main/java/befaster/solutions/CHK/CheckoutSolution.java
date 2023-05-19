372e0f1917d69beccd58ba31708b40a4b70f49e3
sbx_user1051 <sbx_user1051@169.254.199.237>
2023-05-19 14:23:10 +0000

    Fri May 19 14:23:10 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -6,29 +6,33 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public class CheckoutSolution {
 
-    /*
-    CHK_R1
-ROUND 1 - Our supermarket
-The purpose of this challenge is to implement a supermarket checkout that calculates the total price of a number of items.
 
-In a normal supermarket, things are identified using Stock Keeping Units, or SKUs.
-In our store, we'll use individual letters of the alphabet (A, B, C, and so on).
-Our goods are priced individually. In addition, some items are multi-priced: buy n of them, and they'll cost you y pounds.
-For example, item A might cost 50 pounds individually, but this week we have a special offer:
- buy three As and they'll cost you 130.
+
+
+    /*
+    CHK_R2
+ROUND 2 - More offers
+The checkout feature is great and our supermarket is doing fine. Is time to think about growth.
+Our marketing teams wants to experiment with new offer types and we should do our best to support them.
+
+We are going to sell a new item E.
+Normally E costs 40, but if you buy 2 of Es you will get B free. How cool is that ? Multi-priced items also seemed to work well so we should have more of these.
 
 Our price table and offers:
-+------+-------+----------------+
-| Item | Price | Special offers |
-+------+-------+----------------+
-| A    | 50    | 3A for 130     |
-| B    | 30    | 2B for 45      |
-| C    | 20    |                |
-| D    | 15    |                |
-+------+-------+----------------+
++------+-------+------------------------+
+| Item | Price | Special offers         |
++------+-------+------------------------+
+| A    | 50    | 3A for 130, 5A for 200 |
+| B    | 30    | 2B for 45              |
+| C    | 20    |                        |
+| D    | 15    |                        |
+| E    | 40    | 2E get one B free      |
++------+-------+------------------------+
 
 
 Notes:
+ - The policy of the supermarket is to always favor the customer when applying special offers.
+ - All the offers are well balanced so that they can be safely combined.
  - For any illegal input return -1
 
 In order to complete the round you need to implement the following method:
@@ -40,9 +44,6 @@ Where:
 
      */
 
-
-
-
     public Integer checkout(String skus) {
         if (skus == null) return -1;
         if (skus.isEmpty()) return 0;
@@ -52,6 +53,7 @@ Where:
         checkout.put("B", 30);
         checkout.put("C", 20);
         checkout.put("D", 15);
+        checkout.put("E", 15);
 
         final Map<String, Rule> rules = new HashMap<>();
         rules.put("A", new Rule(3, 130));
@@ -110,4 +112,15 @@ Where:
             return promotional + (remaining * originalPrice);
         }
     }
+
+    class SubRule {
+        private final Integer count;
+        private final Integer price;
+
+        public SubRule(final Integer count, final Integer price) {
+            this.count = count;
+            this.price = price;
+        }
+    }
 }
+
