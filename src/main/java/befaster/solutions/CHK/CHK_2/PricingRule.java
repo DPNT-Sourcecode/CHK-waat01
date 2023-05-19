e51653c436e92173b905a011ef3662d79e888837
sbx_user1051 <sbx_user1051@169.254.199.237>
2023-05-19 14:53:10 +0000

    Fri May 19 14:53:10 UTC 2023

--- src/main/java/befaster/solutions/CHK/CHK_2/PricingRule.java
+++ src/main/java/befaster/solutions/CHK/CHK_2/PricingRule.java
@@ -4,17 +4,14 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 public final class PricingRule implements Rule {
-//    private final Collection<SubPricingRule> rules;
-    private final Map<Integer, SubPricingRule> rulesMap = new TreeMap<>(Comparator.reverseOrder());
+    private final TreeMap<Integer, SubPricingRule> rulesMap = new TreeMap<>(Comparator.reverseOrder());
 
     public PricingRule(Collection<SubPricingRule> rules) {
-//        this.rules = rules;
         rules.forEach(rule -> rulesMap.put(rule.getCount(), rule));
     }
 
@@ -37,7 +34,32 @@ public final class PricingRule implements Rule {
 
     @Override
     public int solve(int total, int originalPrice) {
+        if (rulesMap.isEmpty()) return total * originalPrice;
+        if (rulesMap.size() == 1) return rulesMap.pollFirstEntry().getValue().solve(total, originalPrice);
+
+        // Okay, so I need the
+        for (Map.Entry<Integer, SubPricingRule> entry: rulesMap.entrySet()) {
+            // We know the count. I need to know how manytimes this count is satisfied.
+            // If its not satisfied, move to the next rule.
+        }
+
+        rulesMap.forEach((count, rulesMap) -> {
+
+        });
+
+
         return -1;
+//        int timesPromotionIsApplied = (total / count);
+//
+//        if (timesPromotionIsApplied <= 0) {
+//            return total * originalPrice;
+//        }
+//
+//        int promotional = timesPromotionIsApplied * price;
+//        int remaining = total - (count * timesPromotionIsApplied);
+//
+//        return promotional + (remaining * originalPrice);
     }
 }
+
 
